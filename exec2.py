e72d06390393f54332e0cca88a72600475cbac47
romariofarias1812 <romariofarias1812@gmail.com>
2024-08-29 20:45:34 -0300

    Add files via upload

---
+++ exec2.py
@@ -0,0 +1,4 @@
+#2. Etiqueta - Elabore um programa que escreve seu nome na primeira linha, o estado em que você mora na segunda, e seu município na terceira.
+print("Meu nome e Romario Farias da Silva")
+print("Moro no estado do Rio de Janeiro")
+print("no municipio de Duque de Caxias")
